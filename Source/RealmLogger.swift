fcce9a18f6e54421dbe211d0e794fa6477ac2cba
Stan Potemkin <bronenos@gmail.com>
2016-01-09 22:35:01 +0300

    master: try dev-stan-update-existing-change

--- Source/RealmLogger.swift
+++ Source/RealmLogger.swift
@@ -55,7 +55,12 @@ class RealmLogger {
     }
     
     func didUpdate<T: Object>(object: T) {
-        addObject(object, action: .Update)
+        if let change = getChangeForObject(object, action: .Create) {
+            change.updateMirror()
+        }
+        else {
+            addObject(object, action: .Update)
+        }
     }
     
     func didDelete<T: Object>(object: T) {
@@ -71,7 +76,7 @@ class RealmLogger {
     - parameter action Action that was performed on that object
     */
     func addObject<T: Object>(object: T, action: RealmAction) {
-        let realmChange = RealmChange(type: (object as Object).dynamicType, action: action, mirror: object.getMirror())
+        let realmChange = RealmChange(type: (object as Object).dynamicType, action: action, object: object)
         temporary.append(realmChange)
     }
     
@@ -79,5 +84,20 @@ class RealmLogger {
         temporary.removeAll()
     }
     
+    private func getChangeForObject<T: Object>(object: T, action: RealmAction) -> RealmChange? {
+        for change in temporary {
+            if change.action != action {
+                continue
+            }
+            
+            if change.object !== object {
+                continue
+            }
+            
+            return change
+        }
+        
+        return nil
+    }
 }
 
